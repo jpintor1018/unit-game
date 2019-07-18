c8505794b6958571ac68e4da1502af7ea7df943e
John Michael Pintor <jpintor1018@yahoo.com>
2019-07-17 22:01:00 -0400

    Update

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,7 +1,22 @@
+var result=0;
+var wins=0;
+var loss=0;
+
+
 $(document).ready(function(){
 var random = Math.floor(Math.random()*120)+19;
 $(".ranNum").html(' Random Number:      ' + random);
 console.log(random)
+
+
+//IMAGES
+
+var images = [
+    "assets/images/blue crystal.jpg",
+    "assets/images/green crystal.jpg",
+    "assets/images/red crystal.jpg",
+    "assets/images/yellow crystal.jpg"
+]
 
 for( var i =0; i<4;i++) //loop 4 times
     {
@@ -9,14 +24,35 @@ for( var i =0; i<4;i++) //loop 4 times
         crystal.attr("class", 'jewel'); //created a class of 'jewel' in the newly created div
         var ranNumCrystal = Math.floor(Math.random()*13) // generate a random number between 0-12
         crystal.attr("data-random", ranNumCrystal);
+       
         $(".crysImage").append(crystal) //insert content, '<div class="jewel">' into <section class='crysImage>
+        crystal.css({
+            "background-image":"url('" + images[i] + "')",
+            "background-size":"cover"
+        })
     }
 
         $('.jewel').on('click', function(){
 
             var clickedOn = parseInt($(this).attr("data-random"));
-            var result = clickedOn;
-            console.log(result)
+            result += clickedOn;
+            $('.totalScore').html("Your total score is: " + result)
+
+            if( result === random)
+            {
+                console.log("You Win");
+                wins++;
+                $("#wins").html("Wins: " + wins);
+                result=0;
+            }
+            else if( result > random)
+            {
+                console.log("You Lose")
+                loss++;
+                $('#loss').html("Losses: " + loss);
+                result=0;
+            }
+           
         });
 
 
